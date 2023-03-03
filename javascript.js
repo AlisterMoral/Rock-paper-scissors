72d29c480cb15961af1de16af2fbcd432e0d7884
Alister <Alister.moral@gmail.com>
2023-03-03 13:04:10 +0100

    adding function single round and return the winner or looser

--- javascript.js
+++ javascript.js
@@ -5,3 +5,19 @@ function getComputerChoice() {
   }
   console.log(getComputerChoice());
 
+function playRound(playerSelection, computerSelection) {
+ playerSelection = playerSelection.toLowerCase();
+ if (playerSelection === computerSelection) {
+    return "tie!";
+  } else if (
+    (playerSelection === 'rock' && computerSelection === 'scissors') ||
+    (playerSelection === 'paper' && computerSelection === 'rock') ||
+    (playerSelection === 'scissors' && computerSelection === 'paper')
+  ) {
+    return `You Win! ${playerSelection} beats ${computerSelection}`;
+  } else {
+    return `You Lose! ${computerSelection} beats ${playerSelection}`;
+  }
+}
+
+
